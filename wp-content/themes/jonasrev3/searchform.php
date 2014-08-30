016a906b6ce7e18234b9b34dcbe7c222c5fac2f2
Jonas Reitmann <jonas@reitmann.me>
2014-08-30 11:28:54 +0200

    Improved header search
    - better positioning
    - new search icon, now uses HTML code instead of 'x'
    - updated font-weight to bold
    
    Signed-off-by: Jonas Reitmann <jonas@reitmann.me>

--- wp-content/themes/jonasrev3/searchform.php
+++ wp-content/themes/jonasrev3/searchform.php
@@ -1,4 +1,4 @@
 <form method="get" id="search" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 	<input type="text" title="Suchen" placeholder="Suchen" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="searchinput" />
 </form>
-<span class="x">x</span>
+<span class="x">&#x2715;</span>
