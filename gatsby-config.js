56fd495967caf30c0cca9fe1440388fdf379907f
Jonas Reitmann <jonas@reitmann.me>
2020-08-27 17:52:05 +0200

    Add RSS feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
         'gatsby-plugin-styled-components',
         'gatsby-plugin-react-helmet',
         'gatsby-plugin-eslint',
+        `gatsby-plugin-sharp`,
         {
             resolve: `gatsby-source-filesystem`,
             options: {
@@ -17,7 +18,6 @@ module.exports = {
                 name: `posts`
             }
         },
-        `gatsby-plugin-sharp`,
         {
             // There is a bug in gatsby-remark-images
             // Because of this, we add configure the plugin twice.
@@ -39,6 +39,74 @@ module.exports = {
                         options: {
                             maxWidth: 1000
                         }
+                    }
+                ]
+            }
+        },
+        {
+            resolve: `gatsby-plugin-feed-mdx`,
+            options: {
+                query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+                feeds: [
+                    {
+                        serialize: ({ query: { site, allMdx } }) => {
+                            return allMdx.edges.map(edge => {
+                                return Object.assign(
+                                    {},
+                                    edge.node.frontmatter,
+                                    {
+                                        description: edge.node.excerpt,
+                                        date: edge.node.frontmatter.date,
+                                        url:
+                                            site.siteMetadata.siteUrl +
+                                            '/posts' +
+                                            edge.node.frontmatter.slug,
+                                        guid:
+                                            site.siteMetadata.siteUrl +
+                                            '/posts' +
+                                            edge.node.frontmatter.slug,
+                                        custom_elements: [
+                                            {
+                                                'content:encoded':
+                                                    edge.node.html
+                                            }
+                                        ]
+                                    }
+                                );
+                            });
+                        },
+                        query: `
+              {
+                allMdx(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      frontmatter {
+                        title
+                        date
+                        slug
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+                        output: '/rss.xml',
+                        title: 'Jonas Reitmann'
                     }
                 ]
             }
